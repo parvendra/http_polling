4c227b04cccaa0f0f5a759d7d7e8ba10c8502604
parvendra <parvendra.s@multunus.com>
2010-12-18 12:03:57 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HttpPolling::Application.config.secret_token = '440df3eee35f8e1a780195976bd96a535fa454bcd8d32e144d33fc6ad1578af755454122e7b7367ea0562f95ff79e477b7a8c44b4ea10b51627a9dc88805e2ac'
